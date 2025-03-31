bbf2ed87e5f479e73b2149d77cb4860fe5eb6188
ErinZE <erinzeqiraj10@gmail.com>
2025-03-31 16:11:49 +0200

    Ajout du port pour Render

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Mon projet tourne sur Render !");
+const http = require('http');
+
+// Render impose l'utilisation de process.env.PORT
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello depuis Render 🚀');
+});
+
+server.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
